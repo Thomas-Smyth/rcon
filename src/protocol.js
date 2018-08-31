c41f5e6c64ee1f927a320a34d508241d06bc6ff1
Bohdan Didyk <bodink7@gmail.com>
2018-08-31 20:43:32 +0300

    style update

--- src/protocol.js
+++ src/protocol.js
@@ -5,11 +5,11 @@
  * @enum {number}
  */
 module.exports = Object.freeze({
-    SERVERDATA_AUTH: 0x03,              // First packet for authenticate
-    SERVERDATA_EXECCOMMAND: 0x02,       // Execute commands
-    SERVERDATA_AUTH_RESPONSE: 0x02,     // Response of SERVERDATA_AUTH, body === -1 mean failed
-    SERVERDATA_RESPONSE_VALUE: 0x00,    // Response of SERVERDATA_EXECCOMMAND
+  SERVERDATA_AUTH: 0x03,              // First packet for authenticate
+  SERVERDATA_EXECCOMMAND: 0x02,       // Execute commands
+  SERVERDATA_AUTH_RESPONSE: 0x02,     // Response of SERVERDATA_AUTH, body === -1 mean failed
+  SERVERDATA_RESPONSE_VALUE: 0x00,    // Response of SERVERDATA_EXECCOMMAND
 
-    ID_AUTH: 0x999,
-    ID_REQUEST: 0x123
+  ID_AUTH: 0x999,
+  ID_REQUEST: 0x123
 });
